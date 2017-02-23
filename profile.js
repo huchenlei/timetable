371c39f5f056852a3af376177f50196a02243208
chenj209 <Jianda.chen@mail.utoronto.ca>
2017-02-23 15:00:41 -0500

    add try smart button

--- profile.js
+++ profile.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
     $("#show-advanced").on("click", function() {
         $(".advanced-search").toggleClass("hide");
     });
-    
+    $(".smart-button").on("click", function() {
+        $(".right-bar").toggleClass("hide");
+        $(".smart-button").toggleClass("button-position");
+        $(".main").toggleClass("main-size");
+        $(".arrowright").toggleClass("movearrow");
+    }); 
 });
