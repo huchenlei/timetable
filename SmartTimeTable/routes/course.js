4709c8e13ee65c8fd8171738e678a87f62122beb
chenj209 <Jianda.chen@mail.utoronto.ca>
2017-03-17 19:10:02 -0400

    add backend ph3

---
+++ SmartTimeTable/routes/course.js
@@ -0,0 +1,308 @@
+var express = require('express');
+var database = require('../models/database');
+var router = express.Router();
+
+/* A course should consists of departmentID, courseNum, br (breadthRequirement),
+ * timeslot, and courseDescription
+ */
+//Create a course
+function insertCourse(req, res) {
+  var response = {
+      _id: req.body.courseCode,
+      title: req.body.title,
+      description: req.body.description,
+      br: req.body.br
+  };
+  database.courseSchema.count({_id: req.body.courseCode},
+    function(err, count){
+      if (count > 0) {
+        console.log("course exists");
+        return res.sendStatus(400);
+      }
+      else {
+        new database.courseSchema(response).save(function (err, success) {
+          if (err) {
+              console.log(response);
+              return res.sendStatus(400);
+          } else {
+              if (success) {
+                return res.sendStatus(200);
+              }
+          }
+        });
+      }
+    });
+}
+
+//Get a course info
+function getCourseInfo(req, res) {
+  var results = [];
+  database.courseSchema
+    .findOne({
+      _id: req.params.courseCode
+    },
+    function(err, course) {
+      if (err) {
+        return res.sendStatus(400);
+      }
+      else {
+        if (!course) {
+          console.log("No course found.")
+        } else {
+          req.title = course.title;
+          req.br = course.br;
+          req.description = course.description;
+        }
+      }
+    })
+    .populate({
+      path: 'sections',
+      match: {semester: req.params.semester},
+      populate: {
+        path: 'timeslots'
+      }
+    })
+    .exec(function(err, sections){
+      return res.json(sections);
+    });
+}
+
+
+function insertSection(req, res) {
+  var response = {
+      courseCode: req.body.courseCode,
+      semester: req.body.semester,
+      type: req.body.type,
+      sectionCode: req.body.sectionCode,
+      instructor: req.body.instructor
+  };
+  database.courseSchema.findOne({
+    _id: response.courseCode
+  },
+    function(err, course){
+      if (!course) {
+        console.log("course does not exists");
+        return res.sendStatus(400);
+      }
+      else {
+        var newSection = new database.courseSectionSchema(response);
+        course.sections.push(newSection._id);
+        newSection.save(function (err, success) {
+          if (err) {
+              console.log("new section error");
+              console.log(response);
+              return res.sendStatus(400);
+          } else {
+            console.log(course.sections);
+            course.save(function(err, success){
+              if (err) {
+                  console.log("update course error");
+                  return res.sendStatus(400);
+              }
+            });
+            return res.sendStatus(200);
+          }
+        });
+      }
+    });
+}
+
+// database.courseSectionSchema.pre('remove', function(next) {
+//     var query = database.courseTimeslotSchema.remove({cid: this._id});
+//     query.exec(function(err){
+//       if (err) {
+//         var error = new Error("Delete timeslot fail");
+//         next(error);
+//       } else {
+//         next();
+//       }
+//     });
+// });
+// function deleteSection(req, res) {
+//   var response = {
+//       courseCode: req.body.courseCode,
+//       semester: req.body.semester,
+//       type: req.body.type,
+//       sectionCode: req.body.sectionCode
+//   };
+//   database.courseSchema.findOne({
+//       _id: response.courseCode
+//   }).then(function(err, course){
+//       if (err) {
+//         console.log("error: looking for course");
+//         res.sendStatus(400);
+//       }
+//       if (!course) {
+//         console.log("course does not exists");
+//         return res.sendStatus(400);
+//       }
+//     }
+//     database.courseSectionSchema.findOne(responsefunction (err, section) {
+//       if (err) {
+//           console.log("section look up error");
+//           console.log(response);
+//           return res.sendStatus(400);
+//       } else {
+//         if (!section) {
+//           console.log("section does not exists");
+//           return res.sendStatus(400);
+//         }
+//       }
+//     })
+//   ).then(
+//     var index = course.sections.indexOf(section._id);
+//     course.sections.splice(index, 1);
+//     course.save(function(err, success){
+//       if (err) {
+//           console.log("delete course section from course error");
+//           return res.sendStatus(400);
+//       }
+//     });
+//   ).then(
+//     database.courseSectionSchema.findOne(response, function (err) {
+//       if (err) {
+//         console.log("error: delete section tuple");
+//         return res.sendStatus(400);
+//       } else {
+//         return res.json(course);
+//       }
+//     });
+//   );
+// }
+
+function updateSection(req, res) { // currently only instructor update is allowed
+  database.courseSectionSchema.findOne({
+      courseCode: req.body.courseCode,
+      semester: req.body.semester,
+      type: req.body.type,
+      sectionCode: req.body.sectionCode
+  },
+    function(err, section){
+      if (!section) {
+        console.log("course section does not exists");
+        return res.sendStatus(400);
+      }
+      else {
+        section.instructor = req.body.instructor;
+        section.save(function(err, success){
+          if (err) {
+              console.log("update course section error");
+              return res.sendStatus(400);
+          }
+        });
+        return res.json(section);
+      }
+    });
+}
+
+function insertTimeslots(req, res) {
+  var target = {
+    courseCode: req.body.courseCode,
+    semester: req.body.semester,
+    type: req.body.type,
+    sectionCode: req.body.sectionCode
+  };
+  database.courseSectionSchema.findOne(target
+    , function(err, section) {
+    if (err) {
+      console.log("error finding section");
+      return res.sendStatus(400);
+    }
+    if(!section) {
+      console.log("section not found");
+      console.log(target);
+      return res.sendStatus(400);
+    }
+    else {
+      var response = {
+        cid: section._id,
+        weekday: req.body.weekday,
+        start: req.body.start,
+        end: req.body.end,
+        location: req.body.location
+      };
+      var newTime = new database.courseTimeSchema(response);
+      section.timeslots.push(newTime._id);
+      section.save(function(err) {
+        if (err) {
+          console.log("update section fail");
+          return res.sendStatus(400);
+        }
+      });
+      newTime.save(function(err) {
+        if (err) {
+          console.log("update timeslot fail");
+          return res.sendStatus(400);
+        }
+      });
+      return res.sendStatus(200);
+    }
+  })
+}
+
+
+function deleteTimeslot(req, res) {
+  var target = {
+    courseCode: req.body.courseCode,
+    semester: req.body.semester,
+    type: req.body.type,
+    sectionCode: req.body.sectionCode
+  };
+  database.courseSectionSchema.findOne(target
+    , function(err, section) {
+    if (err) {
+      console.log("error finding section");
+      return res.sendStatus(400);
+    }
+    if(!section) {
+      console.log("section not found");
+      console.log(target);
+      return res.sendStatus(400);
+    }
+    else {
+      var response = {
+        cid: section._id,
+        weekday: req.body.weekday,
+        start: req.body.start,
+        location: req.body.location
+      };
+
+      var index = section.timeslots.indexOf(newTime._id);
+      section.splice(index, 1);
+      section.save(function(err) {
+        if (err) {
+          console.log("delete from section fail");
+          return res.sendStatus(400);
+        }
+      });
+      database.courseTimeSchema.remove(response, function(err) {
+        if (err) {
+          console.log("delete timeslot fail");
+          return res.sendStatus(400);
+        }
+      });
+      return res.sendStatus(200);
+    }
+  })
+}
+
+
+
+// courseSchema
+router.post('/insertCourse', insertCourse); 
+router.get('/courseInfo/:semester/:courseCode', getCourseInfo);
+//router.update('/updateCourse', updateCourse);
+
+//router.delete('/deleteCourse', deleteCourse);
+
+// courseSectionSchema
+router.post('/insertSection', insertSection);
+//router.post('/sectionInfo', getSectionInfo);
+router.put('/updateSection', updateSection);
+//router.delete('/deleteSection', deleteSection);
+
+
+router.post('/insertTimeslot', insertTimeslots);
+router.delete('/deleteTimeslot', deleteTimeslot);
+
+module.exports = router;
---
+++ SmartTimeTable/routes/users.js
@@ -0,0 +1,221 @@
+var express = require('express');
+var database = require('../models/database');
+var router = express.Router();
+
+
+//add user, and if username already exists, send 400 status
+function addUser(req, res) {
+
+  var profile = {
+    _id: req.body.userName,
+    passWord: req.body.password,
+    fullName: req.body.fullName,
+    emailAdress: req.body.emailAdress
+
+  };
+
+  database.userSchema.count({_id: req.body.userName},
+  function(err, count){
+    if (count > 0){
+      console.log("username already exists");
+      return res.sendStatus(403);
+    } else {
+      new database.userSchema(profile).save(function (err, success){
+        if (err) {
+          console.log(profile);
+          return res.sendStatus(400);
+        } else {
+          if (success) {
+            return res.sendStatus(200);
+          }
+        }
+      });
+    }
+  });
+}
+
+//get user's info
+function getUserInfo(req, res) {
+  var results = [];
+  database.userSchema.findOne({
+    _id:req.params.userName
+  }, function(err, user) {
+    if (err) {
+      return res.sendStatus(400);
+    } else {
+      if (!user) {
+        console.log("no user found.");
+      } else {
+        // req.fullName = user.fullName;
+        // req.emailAdress = user.emailAdress;
+      }
+    }
+  }).populate({
+    path: 'courses',
+    populate: {
+      path: 'timeslots'
+    }
+  })
+  .populate('preferences')
+  .exec(function(err, userName){
+    return res.json(userName);
+  });
+}
+
+//suspend user
+function suspendUser(req, res) {
+  database.userSchema.count({ _id: req.params.userName},
+  function(err, count){
+    if (err){
+      console.log("error in suspendUser");
+      return res.sendStatus(400);
+    } else {
+      if (count == 0) {
+        console.log("user doesn't exist");
+        return res.sendStatus(401);
+      } else {
+
+        database.userSchema.remove({
+          _id:req.params.userName
+        }, function(err, result){
+          return res.sendStatus(200);
+        });
+          console.log("after delete");
+      }
+    }
+  });
+}
+
+function addUserCourse(req, res) {
+
+  //find user
+  database.userSchema.findOne({
+    _id:req.params.userName
+  }, function(err, user){
+    if (!user) {
+      console.log("usernot found");
+      return res.sendStatus(400);
+    } else {
+      database.courseSectionSchema.findOne({
+        courseCode: req.body.courseCode,
+        sectionCode: req.body.sectionCode,
+        semester: req.body.semester,
+        type: req.body.type
+      }, function(err, section){
+        if (!section){
+          console.log("cannot find section");
+        } else {
+          user.courses.push(section._id);
+          user.save(function(err, success){
+            if (err) {
+              console.log("user saving error");
+              return res.sendStatus(400);
+            } else {
+              console.log("user saving success");
+              return res.json(user);
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+//user.courses.push(course._id)
+//update user info
+function updateUser(req, res) {
+  database.userSchema.count({_id:req.params.userName},
+  function(err, count){
+    if (err) {
+      console.log("error in update");
+      return res.sendStatus(400);
+    } else {
+      if (count == 0) {
+        console.log("user doesn't exist");
+        return res.sendStatus(400);
+      } else {
+
+        var updateJSON = {};
+        if (req.body.passWord) {
+
+          updateJSON.passWord = req.body.passWord;
+        }
+        if (req.body.emailAdress) {
+
+          updateJSON.emailAdress = req.body.emailAdress;
+        }
+        if (req.body.fullName) {
+
+          updateJSON.fullName = req.body.fullName;
+        }
+        database.userSchema.updateOne({
+          _id:req.params.userName
+        }, {
+          $set: updateJSON
+        }, function(err, result){
+          if (err){
+
+            res.sendStatus(403);
+
+          } else {
+
+            res.sendStatus(200);
+          }
+        });
+      }
+    }
+  });
+}
+
+function insertPreference(req, res) {
+  var newPreference = {
+    uid: req.params.userName,
+    type: req.body.type,
+    value: req.body.value
+  };
+  var response = new database.preferenceSchema(newPreference);
+  response.save(function (err) {
+    if (err) {
+      console.log(err.message);
+      return res.sendStatus(400);
+    } else {
+      database.userSchema.findOne({_id: newPreference.uid}, function(err, user) {
+        if (err || !user) {
+          console.log("error finding user");
+          return res.sendStatus(400);
+        } else {
+          user.preferences.push(response._id);
+          user.save(function(err) {
+            if (err) {
+              console.log("error updating user");
+              return res.sendStatus(400);
+            } else {
+              return res.json(user);
+            }
+          });
+        }
+      });
+    }
+  })
+}
+
+function checkUserExist(req, res, next) {
+  database.userSchema.findOne({_id: req.params.userName}, function(err, user) {
+    if (err || !user) {
+      console.log("error: finding user");
+      res.sendStatus(400);
+    } else {
+      next();
+    }
+  })
+}
+
+router.post('/addUser', addUser);
+router.get('/info/:userName', getUserInfo);
+router.delete('/info/:userName', suspendUser);
+router.put('/info/:userName', updateUser);
+router.post('/info/:userName/:courses', addUserCourse);
+
+router.post('/insertPreference/:userName', checkUserExist, insertPreference);
+router.get('/preferenceList/:userName', getPreferenceList);
+module.exports = router;
