4fc14f0cf6b279c175ea3e1e87ef228bdd843e67
chenj209 <Jianda.chen@mail.utoronto.ca>
2017-03-29 12:02:09 -0400

    merge view engine

--- Phase3/app.js
+++ Phase3/app.js
@@ -11,54 +11,25 @@ var courses = require('./routes/course');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-nunjucks.configure(path.join(__dirname, "views"), { autoescape: true, express: app });
+nunjucks.configure(path.join(__dirname, "./views"), { autoescape: true, express: app });
 app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', routes);
 app.use('/courses', courses);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get("/", function(req, res) {
+  res.render("index.html");
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.listen(4000, function () {
+  console.log('listening on port 4000!');
 });
-
-
 
 module.exports = app;
