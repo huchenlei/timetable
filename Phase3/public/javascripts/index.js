d9c6e4937c9b817ff5a8efd974a52ada34bfa6d4
chenbowen <cikefengbao@163.com>
2017-03-31 13:29:38 -0400

    1

--- Phase3/public/javascripts/index.js
+++ Phase3/public/javascripts/index.js
@@ -1,3 +1,414 @@
+var csc108l0101 = {
+	courseCode: "csc108",
+	tittle:'',
+	description:'',
+	sectionCode: "L0101",
+	time: [{
+		weekday: "mon",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 16,
+		end: 17,
+		type: "lec"
+	},{
+		weekday: "wed",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 16,
+		end: 17,
+		type: "lec"
+	},{
+		weekday: "fri",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 16,
+		end: 17,
+		type: "tut"
+	}],
+	score: 5
+}
+var csc108l0102 = {
+	courseCode: "csc108",
+	sectionCode: "L0102",
+	tittle:'',
+	description:'',
+	time: [{
+		weekday: "mon",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 10,
+		end: 11,
+		type: "lec"
+	},{
+		weekday: "wed",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 10,
+		end: 11,
+		type: "lec"
+	},{
+		weekday: "fri",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 10,
+		end: 1,
+		type: "tut"
+	}],
+	score: 4
+}
+var csc108l0201 = {
+	courseCode: "csc108",
+	sectionCode: "L0201",
+	tittle:'',
+	description:'',
+	time: [{
+		weekday: "mon",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 13,
+		end: 14,
+		type: "lec"
+	},{
+		weekday: "wed",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 13,
+		end: 14,
+		type: "lec"
+	},{
+		weekday: "fri",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 13,
+		end: 14,
+		type: "tut"
+	}],
+	score: 3
+}
+var csc108l0501 = {
+	courseCode: "csc108",
+	sectionCode: "L0501",
+	tittle:'',
+	description:'',
+	time: [{
+		weekday: "wed",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 18,
+		end: 21,
+		type: "lec"
+	}],
+	score: 3
+}
+var csc165l0101 = {
+	courseCode: "csc165",
+	sectionCode: "L0101",
+	tittle:'',
+	description:'',
+	time: [{
+		weekday: "mon",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 11,
+		end: 13,
+		type: "lec"
+	},{
+		weekday: "wed",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 11,
+		end: 12,
+		type: "lec"
+	},{
+		weekday: "fri",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 11,
+		end: 13,
+		type: "tut"
+	}],
+	score: 0
+}
+var csc165l0201 = {
+	courseCode: "csc165",
+	sectionCode: "L0201",
+	tittle:'',
+	description:'',
+	time: [{
+		weekday: "mon",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 14,
+		end: 16,
+		type: "lec"
+	},{
+		weekday: "wed",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 12,
+		end: 13,
+		type: "lec"
+	},{
+		weekday: "fri",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 14,
+		end: 16,
+		type: "tut"
+	}],
+	score: 0
+}
+var mat137l0101 = {
+	courseCode: "mat137",
+	sectionCode: "L0101",
+	tittle:'',
+	description:'',
+	time: [{
+		weekday: "mon",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 9,
+		end: 10,
+		type: "lec"
+	},{
+		weekday: "wed",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 9,
+		end: 10,
+		type: "lec"
+	},{
+		weekday: "fri",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 9,
+		end: 10,
+		type: "tut"
+	}],
+	score: 0
+}
+var mat137l0201 = {
+	courseCode: "mat137",
+	sectionCode: "L0201",
+	tittle:'',
+	description:'',
+	time: [{
+		weekday: "mon",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 10,
+		end: 11,
+		type: "lec"
+	},{
+		weekday: "wed",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 10,
+		end: 11,
+		type: "lec"
+	},{
+		weekday: "fri",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 10,
+		end: 11,
+		type: "tut"
+	}],
+	score: 0
+}
+var mat223l0101 = {
+	courseCode: "mat223",
+	sectionCode: "L0101",
+	tittle:'',
+	description:'',
+	time: [{
+		weekday: "tue",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 10,
+		end: 12,
+		type: "lec"
+	},{
+		weekday: "wed",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 14,
+		end: 15,
+		type: "tut"
+	}],
+	score: 0
+}
+var mat223l0102 = {
+	courseCode: "mat223",
+	sectionCode: "L0102",
+	tittle:'',
+	description:'',
+	time: [{
+		weekday: "tue",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 10,
+		end: 12,
+		type: "lec"
+	},{
+		weekday: "wed",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 13,
+		end: 14,
+		type: "tut"
+	}],
+	score: 0
+}
+var phl245l0101 = {
+	courseCode: "phl245",
+	sectionCode: "L0101",
+	time: [{
+		weekday: "mon",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 14,
+		end: 16,
+		type: "lec"
+	},{
+		weekday: "wed",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 14,
+		end: 15,
+		type: "lec"
+	}],
+	score: 0
+}
+var phl246l5101 = {
+	courseCode: "phl246",
+	sectionCode: "L5101",
+	tittle:'',
+	description:'',
+	time: [{
+		weekday: "mon",
+		instructor: "Dianne",
+		location: "BA1070",
+		start: 18,
+		end: 21,
+		type: "lec"
+	}],
+	score: 0
+}
+var solutionlist = [
+		[csc108l0101, csc165l0101, mat137l0101, mat223l0102, phl245l0101, phl246l5101], // solution_one, best
+		[csc108l0101, csc165l0101, mat137l0201, mat223l0102, phl245l0101, phl246l5101], // solution_two
+		[csc108l0102, csc165l0101, mat137l0101, mat223l0102, phl245l0101, phl246l5101], // solution_three
+		[csc108l0501, csc165l0101, mat137l0101, mat223l0102, phl245l0101, phl246l5101], // solution_four
+		[csc108l0501, csc165l0101, mat137l0201, mat223l0102, phl245l0101, phl246l5101]	// solution_five, worse
+	];
+// index of current solution
+var cur = 0;
+var currentlist = solutionlist[0];
+var convert_weekday = { "mon": 1,"tue": 2,"wed": 3,"thu": 4,"fri": 5};
+var back = ["PaleGoldenRod","lightblue","LightSalmon", "lightgreen", "lightpink", "Chocolate", "GreenYellow", "GoldenRod"];
+var option_back = ["MediumPurple", "Fuchsia", "Aqua"];
+// count number for option
+var cnt;
+// check if two course overlap
+var over_lap = function(a, b) {
+	for (var i = 0; i < a.length; i++) {
+		for (var j = 0; j < b.length; j++) {
+			if (a[i].weekday == b[j].weekday && 
+				Math.max(a[i].start, b[j].start) 
+				< Math.min(a[i].end, b[j].end))
+				return true;
+		}
+	}
+	return false;
+}
+
+var draw_option = function(course) {
+	var color = option_back[Math.floor(Math.random() * option_back.length)];
+    for (var i = 0; i < course.time.length; i++) {
+    	var col_num = convert_weekday[course.time[i].weekday];
+    	for (var j = course.time[i].start-7; j < course.time[i].end-7; j++) {
+    		var k = $(".timetable tbody tr:nth-of-type(" + j + ") td:eq(" + col_num + ")");
+    		if (k.hasClass("course"))
+    			return;
+    		k.addClass("course").css("background-color", color);
+    		if (j == course.time[i].start-7) {
+    			k.text("option " + cnt).on("click", function() {
+    				for (var i = 0; i < currentlist.length; i++)
+    					if (currentlist[i].courseCode == course.courseCode) {
+    						currentlist.splice(i, 1, course);
+    						clear_table();
+    						render_solution(cur);
+    						break;
+    					}
+    			});
+    		}
+    	}
+	}
+	cnt++;
+}
+
+var draw_course = function(course) {
+	if (course.hasOwnProperty('color') == false) 
+		course.color = back[Math.floor(Math.random() * back.length)]
+    for (var i = 0; i < course.time.length; i++) {
+    	var col_num = convert_weekday[course.time[i].weekday];
+    	for (var j = course.time[i].start-7; j < course.time[i].end-7; j++) {
+    		var k = $(".timetable tbody tr:nth-of-type(" + j + ") td:eq(" + col_num + ")");
+    		k.addClass("course").css("background-color", course.color);
+    		if (j == course.time[i].start-7) {
+    			k.text(course.courseCode);
+    			k.on("click", function() {
+    				clear_table();
+    				render_solution(cur);
+    				cnt = 1;
+    				for (var i = 0; i < solutionlist.length; i++) 
+    					for (var j = 0; j < solutionlist[i].length; j++) {
+    						if (solutionlist[i][j].courseCode != course.courseCode ||
+    							solutionlist[i][j] == course)
+    							continue;
+    						var success = true;
+    						for (var k = 0; k < currentlist.length; k++)
+    							if (over_lap(solutionlist[i][j], currentlist[k])) {
+    								success = false;
+    								break;
+    							}
+    						if (success) 
+    							draw_option(solutionlist[i][j]);
+    					}
+    			});
+    		}
+    	}
+	}
+}
+var remove_course = function(course) {
+    for (var i = 0; i < course.time.length; i++) {
+    	var col_num = convert_weekday[course.time[i].weekday];
+    	for (var j = course.time[i].start-7; j < course.time[i].end-7; j++) {
+    		var k = $(".timetable tbody tr:nth-of-type(" + j + ") td:eq(" + col_num + ")");
+    		k.removeClass("course").css("background-color", "white");
+    		if (j == course.time[i].start-7)
+    			k.text("").unbind("click");
+    	}
+	}
+}
+
+var clear_table = function() {
+	var k = $(".timetable tbody td");
+    k.removeClass("course").css("background-color", "white");
+    k.text("").unbind("click");
+}
+
+var render_solution = function(index) {
+	var solution = solutionlist[index];
+	for (var i = 0; i < currentlist.length; i++) 
+		remove_course(currentlist[i]);
+	currentlist = solution;
+	cur = index;
+	for (var i = 0; i < solution.length; i++) 
+		draw_course(solution[i]);
+}
+
 $(document).ready(function(){
     $("#show-advanced").on("click", function() {
         $(".advanced-search").toggleClass("hide");
@@ -7,5 +418,15 @@ $(document).ready(function(){
         $("#smart").toggleClass("button-position");
         $(".main").toggleClass("main-size");
         $(".arrowright").toggleClass("movearrow");
-    }); 
+    });
+  	$("#solutions td").on("click", function() {
+  		render_solution($("#solutions td").index(this));
+  	});
+	$("#switch-left").on("click",function() {
+		render_solution((cur-1+solutionlist.length)%solutionlist.length);
+	});
+	$("#switch-right").on("click",function() {
+		render_solution((cur+1)%solutionlist.length);
+	});
+	render_solution(cur);
 });
