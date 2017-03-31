accf6ae5e55358a4b6e8f513b8cfe067f29e486b
chenbowen <cikefengbao@163.com>
2017-03-31 15:00:40 -0400

    finish smart solution

--- Phase3/public/javascripts/index.js
+++ Phase3/public/javascripts/index.js
@@ -1,9 +1,9 @@
 var csc108l0101 = {
-	courseCode: "csc108",
+	_id: "csc108",
 	tittle:'',
 	description:'',
 	sectionCode: "L0101",
-	time: [{
+	timeslots: [{
 		weekday: "mon",
 		instructor: "Dianne",
 		location: "BA1070",
@@ -28,11 +28,11 @@ var csc108l0101 = {
 	score: 5
 }
 var csc108l0102 = {
-	courseCode: "csc108",
+	_id: "csc108",
 	sectionCode: "L0102",
 	tittle:'',
 	description:'',
-	time: [{
+	timeslots: [{
 		weekday: "mon",
 		instructor: "Dianne",
 		location: "BA1070",
@@ -57,11 +57,11 @@ var csc108l0102 = {
 	score: 4
 }
 var csc108l0201 = {
-	courseCode: "csc108",
+	_id: "csc108",
 	sectionCode: "L0201",
 	tittle:'',
 	description:'',
-	time: [{
+	timeslots: [{
 		weekday: "mon",
 		instructor: "Dianne",
 		location: "BA1070",
@@ -86,11 +86,11 @@ var csc108l0201 = {
 	score: 3
 }
 var csc108l0501 = {
-	courseCode: "csc108",
+	_id: "csc108",
 	sectionCode: "L0501",
 	tittle:'',
 	description:'',
-	time: [{
+	timeslots: [{
 		weekday: "wed",
 		instructor: "Dianne",
 		location: "BA1070",
@@ -101,11 +101,11 @@ var csc108l0501 = {
 	score: 3
 }
 var csc165l0101 = {
-	courseCode: "csc165",
+	_id: "csc165",
 	sectionCode: "L0101",
 	tittle:'',
 	description:'',
-	time: [{
+	timeslots: [{
 		weekday: "mon",
 		instructor: "Dianne",
 		location: "BA1070",
@@ -130,11 +130,11 @@ var csc165l0101 = {
 	score: 0
 }
 var csc165l0201 = {
-	courseCode: "csc165",
+	_id: "csc165",
 	sectionCode: "L0201",
 	tittle:'',
 	description:'',
-	time: [{
+	timeslots: [{
 		weekday: "mon",
 		instructor: "Dianne",
 		location: "BA1070",
@@ -159,11 +159,11 @@ var csc165l0201 = {
 	score: 0
 }
 var mat137l0101 = {
-	courseCode: "mat137",
+	_id: "mat137",
 	sectionCode: "L0101",
 	tittle:'',
 	description:'',
-	time: [{
+	timeslots: [{
 		weekday: "mon",
 		instructor: "Dianne",
 		location: "BA1070",
@@ -188,11 +188,11 @@ var mat137l0101 = {
 	score: 0
 }
 var mat137l0201 = {
-	courseCode: "mat137",
+	_id: "mat137",
 	sectionCode: "L0201",
 	tittle:'',
 	description:'',
-	time: [{
+	timeslots: [{
 		weekday: "mon",
 		instructor: "Dianne",
 		location: "BA1070",
@@ -217,11 +217,11 @@ var mat137l0201 = {
 	score: 0
 }
 var mat223l0101 = {
-	courseCode: "mat223",
+	_id: "mat223",
 	sectionCode: "L0101",
 	tittle:'',
 	description:'',
-	time: [{
+	timeslots: [{
 		weekday: "tue",
 		instructor: "Dianne",
 		location: "BA1070",
@@ -239,11 +239,11 @@ var mat223l0101 = {
 	score: 0
 }
 var mat223l0102 = {
-	courseCode: "mat223",
+	_id: "mat223",
 	sectionCode: "L0102",
 	tittle:'',
 	description:'',
-	time: [{
+	timeslots: [{
 		weekday: "tue",
 		instructor: "Dianne",
 		location: "BA1070",
@@ -261,9 +261,9 @@ var mat223l0102 = {
 	score: 0
 }
 var phl245l0101 = {
-	courseCode: "phl245",
+	_id: "phl245",
 	sectionCode: "L0101",
-	time: [{
+	timeslots: [{
 		weekday: "mon",
 		instructor: "Dianne",
 		location: "BA1070",
@@ -281,11 +281,11 @@ var phl245l0101 = {
 	score: 0
 }
 var phl246l5101 = {
-	courseCode: "phl246",
+	_id: "phl246",
 	sectionCode: "L5101",
 	tittle:'',
 	description:'',
-	time: [{
+	timeslots: [{
 		weekday: "mon",
 		instructor: "Dianne",
 		location: "BA1070",
@@ -302,9 +302,11 @@ var solutionlist = [
 		[csc108l0501, csc165l0101, mat137l0101, mat223l0102, phl245l0101, phl246l5101], // solution_four
 		[csc108l0501, csc165l0101, mat137l0201, mat223l0102, phl245l0101, phl246l5101]	// solution_five, worse
 	];
+	
+//var solutionlist;
 // index of current solution
-var cur = 0;
-var currentlist = solutionlist[0];
+var cur;
+var currentlist;
 var convert_weekday = { "mon": 1,"tue": 2,"wed": 3,"thu": 4,"fri": 5};
 var back = ["PaleGoldenRod","lightblue","LightSalmon", "lightgreen", "lightpink", "Chocolate", "GreenYellow", "GoldenRod"];
 var option_back = ["MediumPurple", "Fuchsia", "Aqua"];
@@ -321,21 +323,21 @@ var over_lap = function(a, b) {
 		}
 	}
 	return false;
-}
+};
 
 var draw_option = function(course) {
 	var color = option_back[Math.floor(Math.random() * option_back.length)];
-    for (var i = 0; i < course.time.length; i++) {
-    	var col_num = convert_weekday[course.time[i].weekday];
-    	for (var j = course.time[i].start-7; j < course.time[i].end-7; j++) {
+    for (var i = 0; i < course.timeslots.length; i++) {
+    	var col_num = convert_weekday[course.timeslots[i].weekday];
+    	for (var j = course.timeslots[i].start-7; j < course.timeslots[i].end-7; j++) {
     		var k = $(".timetable tbody tr:nth-of-type(" + j + ") td:eq(" + col_num + ")");
     		if (k.hasClass("course"))
     			return;
     		k.addClass("course").css("background-color", color);
-    		if (j == course.time[i].start-7) {
+    		if (j == course.timeslots[i].start-7) {
     			k.text("option " + cnt).on("click", function() {
     				for (var i = 0; i < currentlist.length; i++)
-    					if (currentlist[i].courseCode == course.courseCode) {
+    					if (currentlist[i]._id == course._id) {
     						currentlist.splice(i, 1, course);
     						clear_table();
     						render_solution(cur);
@@ -346,25 +348,25 @@ var draw_option = function(course) {
     	}
 	}
 	cnt++;
-}
+};
 
 var draw_course = function(course) {
 	if (course.hasOwnProperty('color') == false) 
 		course.color = back[Math.floor(Math.random() * back.length)]
-    for (var i = 0; i < course.time.length; i++) {
-    	var col_num = convert_weekday[course.time[i].weekday];
-    	for (var j = course.time[i].start-7; j < course.time[i].end-7; j++) {
+    for (var i = 0; i < course.timeslots.length; i++) {
+    	var col_num = convert_weekday[course.timeslots[i].weekday];
+    	for (var j = course.timeslots[i].start-7; j < course.timeslots[i].end-7; j++) {
     		var k = $(".timetable tbody tr:nth-of-type(" + j + ") td:eq(" + col_num + ")");
     		k.addClass("course").css("background-color", course.color);
-    		if (j == course.time[i].start-7) {
-    			k.text(course.courseCode);
+    		if (j == course.timeslots[i].start-7) {
+    			k.text(course._id);
     			k.on("click", function() {
     				clear_table();
     				render_solution(cur);
     				cnt = 1;
     				for (var i = 0; i < solutionlist.length; i++) 
     					for (var j = 0; j < solutionlist[i].length; j++) {
-    						if (solutionlist[i][j].courseCode != course.courseCode ||
+    						if (solutionlist[i][j]._id != course._id ||
     							solutionlist[i][j] == course)
     							continue;
     						var success = true;
@@ -380,24 +382,24 @@ var draw_course = function(course) {
     		}
     	}
 	}
-}
+};
 var remove_course = function(course) {
-    for (var i = 0; i < course.time.length; i++) {
-    	var col_num = convert_weekday[course.time[i].weekday];
-    	for (var j = course.time[i].start-7; j < course.time[i].end-7; j++) {
+    for (var i = 0; i < course.timeslots.length; i++) {
+    	var col_num = convert_weekday[course.timeslots[i].weekday];
+    	for (var j = course.timeslots[i].start-7; j < course.timeslots[i].end-7; j++) {
     		var k = $(".timetable tbody tr:nth-of-type(" + j + ") td:eq(" + col_num + ")");
     		k.removeClass("course").css("background-color", "white");
-    		if (j == course.time[i].start-7)
+    		if (j == course.timeslots[i].start-7)
     			k.text("").unbind("click");
     	}
 	}
-}
+};
 
 var clear_table = function() {
-	var k = $(".timetable tbody td");
+	var k = $(".timetable tbody tr").find("td:gt(0)");
     k.removeClass("course").css("background-color", "white");
     k.text("").unbind("click");
-}
+};
 
 var render_solution = function(index) {
 	var solution = solutionlist[index];
@@ -407,7 +409,7 @@ var render_solution = function(index) {
 	cur = index;
 	for (var i = 0; i < solution.length; i++) 
 		draw_course(solution[i]);
-}
+};
 
 $(document).ready(function(){
     $("#show-advanced").on("click", function() {
@@ -419,14 +421,24 @@ $(document).ready(function(){
         $(".main").toggleClass("main-size");
         $(".arrowright").toggleClass("movearrow");
     });
-  	$("#solutions td").on("click", function() {
-  		render_solution($("#solutions td").index(this));
-  	});
-	$("#switch-left").on("click",function() {
-		render_solution((cur-1+solutionlist.length)%solutionlist.length);
-	});
-	$("#switch-right").on("click",function() {
-		render_solution((cur+1)%solutionlist.length);
-	});
-	render_solution(cur);
+    $("#getSolutions").on("click", function() {
+    	//$.get('/smart', function(data) {
+           	//solutionlist = JSON.parse(data);
+           	cur = 0;
+           	currentlist = solutionlist[cur];
+           	render_solution(cur);
+           	$("#solutions table").empty();
+           	for (var i = 0; i < solutionlist.length; i++)
+           		$("#solutions table").append("<tr class='solution'><td>Solution " + (i+1) + "</td></tr>");
+           	$("#solutions td").on("click", function() {
+  				render_solution($("#solutions td").index(this));
+  			});
+  			$("#switch-left").on("click",function() {
+				render_solution((cur-1+solutionlist.length)%solutionlist.length);
+			});
+			$("#switch-right").on("click",function() {
+				render_solution((cur+1)%solutionlist.length);
+			});
+        //});
+    });
 });
---
+++ Phase3/routes copy/course.js
@@ -0,0 +1,272 @@
+var express = require('express');
+var database = require('../models/database');
+var router = express.Router();
+
+/* A course should consists of departmentID, courseNum, br (breadthRequirement),
+ * timeslot, and courseDescription
+ */
+//Create a course
+function insertCourse(req, res) {
+  var response = {
+      _id: req.body.courseCode,
+      title: req.body.title,
+      description: req.body.description,
+      br: req.body.br
+  };
+  database.courseSchema.count({_id: req.body.courseCode},
+    function(err, count){
+      if (count > 0) {
+        console.log("course exists");
+        return res.sendStatus(400);
+      }
+      else {
+        new database.courseSchema(response).save(function (err, success) {
+          if (err) {
+              console.log(response);
+              return res.sendStatus(400);
+          } else {
+              if (success) {
+                return res.sendStatus(200);
+              }
+          }
+        });
+      }
+    });
+}
+
+//Get a course info
+function getCourseInfo(req, res) {
+  var results = [];
+  database.courseSchema
+    .findOne({
+      _id: req.params.courseCode
+    },
+    function(err, course) {
+      if (err) {
+        return res.sendStatus(400);
+      }
+      else {
+        if (!course) {
+          console.log("No course found.")
+        } else {
+          req.title = course.title;
+          req.br = course.br;
+          req.description = course.description;
+        }
+      }
+    })
+    .populate({
+      path: 'sections',
+      match: {semester: req.params.semester},
+      populate: {
+        path: 'timeslots'
+      }
+    })
+    .exec(function(err, sections){
+      return res.json(sections);
+    });
+}
+
+
+function insertSection(req, res) {
+  var response = {
+      courseCode: req.body.courseCode,
+      semester: req.body.semester,
+      type: req.body.type,
+      sectionCode: req.body.sectionCode,
+      instructor: req.body.instructor
+  };
+  database.courseSchema.findOne({
+    _id: response.courseCode
+  },
+    function(err, course){
+      if (!course) {
+        console.log("course does not exists");
+        return res.sendStatus(400);
+      }
+      else {
+        var newSection = new database.courseSectionSchema(response);
+        course.sections.push(newSection._id);
+        newSection.save(function (err, success) {
+          if (err) {
+              console.log("new section error");
+              console.log(response);
+              return res.sendStatus(400);
+          } else {
+            console.log(course.sections);
+            course.save(function(err, success){
+              if (err) {
+                  console.log("update course error");
+                  return res.sendStatus(400);
+              }
+            });
+            return res.sendStatus(200);
+          }
+        });
+      }
+    });
+}
+
+function deleteSection(req, res) {
+  var response = {
+      courseCode: req.body.courseCode,
+      semester: req.body.semester,
+      type: req.body.type,
+      sectionCode: req.body.sectionCode
+  };
+  courseSectionSchema.remove(response, function(err) {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(400);
+    }
+    return res.sendStatus(200);
+  });
+}
+
+function updateSection(req, res) { // currently only instructor update is allowed
+  database.courseSectionSchema.findOne({
+      courseCode: req.body.courseCode,
+      semester: req.body.semester,
+      type: req.body.type,
+      sectionCode: req.body.sectionCode
+  },
+    function(err, section){
+      if (!section) {
+        console.log("course section does not exists");
+        return res.sendStatus(400);
+      }
+      else {
+        section.instructor = req.body.instructor;
+        section.save(function(err, success){
+          if (err) {
+              console.log("update course section error");
+              return res.sendStatus(400);
+          }
+        });
+        return res.json(section);
+      }
+    });
+}
+
+function insertTimeslots(req, res) {
+  var target = {
+    courseCode: req.body.courseCode,
+    semester: req.body.semester,
+    type: req.body.type,
+    sectionCode: req.body.sectionCode
+  };
+  database.courseSectionSchema.findOne(target
+    , function(err, section) {
+    if (err) {
+      console.log("error finding section");
+      return res.sendStatus(400);
+    }
+    if(!section) {
+      console.log("section not found");
+      console.log(target);
+      return res.sendStatus(400);
+    }
+    else {
+      var response = {
+        cid: section._id,
+        weekday: req.body.weekday,
+        start: req.body.start,
+        end: req.body.end,
+        location: req.body.location
+      };
+      var newTime = new database.courseTimeSchema(response);
+      section.timeslots.push(newTime._id);
+      section.save(function(err) {
+        if (err) {
+          console.log("update section fail");
+          return res.sendStatus(400);
+        } else {
+          newTime.save(function(err) {
+            if (err) {
+              console.log("update timeslot fail");
+              return res.sendStatus(400);
+            } else {
+              return res.sendStatus(200);
+            }
+          });
+        }
+      });
+    }
+  })
+}
+
+
+function deleteTimeslot(req, res) {
+  var target = {
+    courseCode: req.body.courseCode,
+    semester: req.body.semester,
+    type: req.body.type,
+    sectionCode: req.body.sectionCode
+  };
+  database.courseSectionSchema.findOne(target
+    , function(err, section) {
+    if (err) {
+      console.log("error finding section");
+      return res.sendStatus(400);
+    }
+    if(!section) {
+      console.log("section not found");
+      console.log(target);
+      return res.sendStatus(400);
+    }
+    else {
+      var response = {
+        cid: section._id,
+        weekday: req.body.weekday,
+        start: req.body.start,
+        location: req.body.location
+      };
+      database.courseTimeSchema.findOne(response, function(err, time){
+        if (err) {
+          console.log("error finding the time");
+          res.sendStatus(400);
+        } else {
+          var index = section.timeslots.indexOf(time._id);
+          section.timeslots.splice(index, 1);
+          section.save(function(err) {
+            if (err) {
+              console.log("delete from section fail");
+              return res.sendStatus(400);
+            } else {
+              database.courseTimeSchema.remove(response, function(err) {
+                if (err) {
+                  console.log("delete timeslot fail");
+                  return res.sendStatus(400);
+                } else {
+                  return res.sendStatus(200);
+                }
+              });
+            }
+          });
+        }
+      });
+      
+    }
+  })
+}
+
+
+
+// courseSchema
+router.post('/insertCourse', insertCourse); 
+router.get('/courseInfo/:semester/:courseCode', getCourseInfo);
+//router.update('/updateCourse', updateCourse);
+
+//router.delete('/deleteCourse', deleteCourse);
+
+// courseSectionSchema
+router.post('/insertSection', insertSection);
+//router.post('/sectionInfo', getSectionInfo);
+router.put('/updateSection', updateSection);
+router.delete('/deleteSection', deleteSection);
+
+
+router.post('/insertTimeslot', insertTimeslots);
+router.delete('/deleteTimeslot', deleteTimeslot);
+
+module.exports = router;
