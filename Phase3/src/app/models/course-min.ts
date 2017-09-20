3f1fe2b0a4e0a2fb437d2ce9fd11211603722785
CharlieUofT <chenlei.hu@mail.utoronto.ca>
2017-09-19 20:37:42 -0400

    Delete unused files; Reorganized docs

--- Phase3/src/app/models/course-min.ts
+++
@@ -1,8 +0,0 @@
-import { Section } from './section';
-import { Time } from './time';
-
-export class CourseMin {
-  code : string[];
-  courseCode : string;
-  times : Time[]
-}
--- Phase3/src/app/models/section.ts
+++
@@ -1,8 +0,0 @@
-import { Time } from './time';
-export class Section{
-    "code": string;
-    "instructors": string[];
-    "times": Time[];
-    "size": number;
-    "enrolment": number;
-}
